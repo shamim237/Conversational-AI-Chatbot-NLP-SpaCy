db68ad53fedccfb12deeb44c998885c5f884fb3d
Shamim Mahbub <jiboneee237@gmail.com>
2022-09-11 23:19:54 +0600

    reminder_checking_04

--- dialogs/adv_pill_remind_dialog.py
+++ dialogs/adv_pill_remind_dialog.py
@@ -85,7 +85,7 @@ class AdvPillReminderDialog(ComponentDialog):
         main = wks.acell("A2").value
         wks.update_acell("E1", main)
 
-        last = step_context.options
+        last = step_context.context.activity
         wks.update_acell("A5", str(last))
 
         pred = reminder_class(main)
