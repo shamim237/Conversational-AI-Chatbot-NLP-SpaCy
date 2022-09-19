5fe0e5cd19f7f235d625db2c306e824da8ef4f68
Shamim Mahbub <jiboneee237@gmail.com>
2022-09-19 15:25:01 +0600

    translation

--- nlp_model/appoint_predict.py
+++ nlp_model/appoint_predict.py
@@ -4,5 +4,5 @@ def predict_appoint(msg):
     res = requests.get('https://appoint-spacy.herokuapp.com/predict/{}'.format(msg))
     return res.json()
 
-ss = predict_appoint("Book an appointment on 23 sept")
-print(ss)
+# ss = predict_appoint("Book an appointment on 23 sept")
+# print(ss)
