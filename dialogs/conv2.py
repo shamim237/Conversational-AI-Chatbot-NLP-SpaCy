e1f02a6ce3e4c65e0ff94a4a5fbfd496f5146db1
Shamim Mahbub <jiboneee237@gmail.com>
2022-10-20 17:17:32 +0600

    conv9

--- dialogs/conv2.py
+++ dialogs/conv2.py
@@ -124,6 +124,24 @@ class Conv2Dialog(ComponentDialog):
         else:
             prompts = "nothing understand"
             resp = response(step_context.result)
-            await step_context.context.send_activity(
-                MessageFactory.text(resp, extra = step_context.result))
-            return await step_context.begin_dialog("conv1") 
+            if len(resp) == 1:
+                return await step_context.prompt(
+                    TextPrompt.__name__,
+                    PromptOptions(
+                        prompt=MessageFactory.text(resp[0], extra = step_context.context.activity.text)),)
+            if len(resp) == 2:
+                await step_context.context.send_activity(
+                    MessageFactory.text(resp[0], extra = main))                        
+                return await step_context.prompt(
+                    TextPrompt.__name__,
+                    PromptOptions(
+                        prompt=MessageFactory.text(resp[1], extra = step_context.context.activity.text)),)
+            if len(resp) == 3:
+                await step_context.context.send_activity(
+                    MessageFactory.text(resp[0], extra = main)) 
+                await step_context.context.send_activity(
+                    MessageFactory.text(resp[1], extra = main))                         
+                return await step_context.prompt(
+                    TextPrompt.__name__,
+                    PromptOptions(
+                        prompt=MessageFactory.text(resp[2], extra = step_context.context.activity.text)),)
--- nlp_model/predict.py
+++ nlp_model/predict.py
@@ -11,15 +11,12 @@ def predict_class(msg):
         res = requests.get('https://jarvis-chatbot-model.herokuapp.com/predict/{}'.format(msg))
 
     return res.text
-# ss = predict_class("book an appointment on today")
-# print(ss)
-
 
 def response(msg):
     raw  = translator.detect(msg)
     if raw.lang != "en":
         convert = translator.translate(msg, dest= "en")
-        res = requests.get('https://contextual-jarvis.herokuapp.com/predict/{}'.format(convert.text))
+        res = requests.get('https://jarvis-chatbot-model.herokuapp.com/response/{}'.format(convert.text))
     else:
-        res = requests.get('https://contextual-jarvis.herokuapp.com/predict/{}'.format(msg))
+        res = requests.get('https://jarvis-chatbot-model.herokuapp.com/response/{}'.format(msg))
     return res.text
